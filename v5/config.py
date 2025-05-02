36c64fa4673828ec43c0c4b0a7e563c369c95fce
marc-la <23857377@student.uwa.edu.au>
2025-05-02 11:24:45 +0800

    idk anymore

---
+++ v5/config.py
@@ -0,0 +1,9 @@
+# Variables
+HOST = "127.0.0.1"
+PORT = 5001
+MAX_CONNECTIONS = 12
+INPUT_TIMEOUT = 30
+TIMEOUT_SECONDS = 30
+
+import logging
+logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(message)s')
